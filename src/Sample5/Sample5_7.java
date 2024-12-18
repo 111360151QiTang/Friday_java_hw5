c130a146dd8d6adcc1596d328c3c682739ad4f82
111360151QiTang <t111360151@ntut.org.tw>
2024-12-19 01:26:55 +0800

    first

---
+++ src/Sample5/Sample5_7.java
@@ -0,0 +1,26 @@
+package Sample5;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+class Sample5_7 {
+    public static void main(String[] args){
+        try{
+            FileWriter fw = new FileWriter("test1.txt");
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            
+            pw.println("Hello");
+            pw.println("GoodBye");
+            
+            pw.close();
+            
+            System.out.println("資料已寫入檔案");
+        }
+        
+        catch (IOException e){
+            System.out.println("輸出入有誤");
+        }
+    }
+}
